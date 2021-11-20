09d324306556fe124881a28aa1bcb91e52c3344d
andika.pelealu90 <andika.pelealu90@gmail.com>
2021-11-20 19:15:45 +0700

    remove build

--- build/src/connections/socket.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SocketInit = exports.useSocket = void 0;
-const SocketInit = (socketServer) => {
-    exports.useSocket = socketServer;
-};
-exports.SocketInit = SocketInit;
--- build/src/constants/general.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// response status
-const RESPONSE_STATUS_SUCCESS = `Success`;
-const RESPONSE_STATUS_FAILED = `Failed`;
-exports.default = {
-    RESPONSE_STATUS_SUCCESS,
-    RESPONSE_STATUS_FAILED,
-};
